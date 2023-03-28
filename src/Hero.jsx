95a379e4d91db88dce2edc6b1068d9fba16718cd
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-03-28 14:08:55 +0600

    implemented header section

---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      this is hero section
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/layout/Header';
+import Hero from '../Hero';
 
 const HomePage = () => {
-    return (
-        <div>
-          <Header/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
